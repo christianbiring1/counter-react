9d965a457f16bf6e96f31238f2460ca46502bfa7
christian <christianbiringanine22@gmail.com>
2022-11-28 16:40:54 +0200

    Raise handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -38,6 +38,8 @@ class Counter extends Component {
       <div>
       <span style = {this.styles} className={this.getBadgeColor()}>{this.formatCount()}</span>
       <button className='btn btn-secondary btn-sm' onClick={ this.handleIncrement }>Increment</button>
+      <button className='btn btn-danger btn-sm m-2' onClick={ () => this.props.onDelete(this.props.id) }>Delete</button>
+
       </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,11 +9,18 @@ class Counters extends Component {
       {id: 3, value: 0 },
       {id: 4, value: 0 },
     ]
-  } 
+  }
+
+  handleDelete = (counterId) => {
+    console.log(counterId)
+    const counters = this.state.counters.filter(counter => counter.id !== counterId);
+    this.setState( { counters });
+    console.log(counters)
+  }
   render() { 
     return (
       <div>
-        { this.state.counters.map((counter) => <Counter key={counter.id} value={counter.value} />)}
+        { this.state.counters.map((counter) => <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}/>)}
       </div>
     );
   }
