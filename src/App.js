03ea9e5726baa72953fbd2131b44f874a7b44193
christian <christianbiringanine22@gmail.com>
2022-11-29 12:59:49 +0200

    Lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+
+class App extends Component {
+  state = {
+    counters: [
+      {id: 1, value: 4 },
+      {id: 2, value: 0 },
+      {id: 3, value: 0 },
+      {id: 4, value: 0 },
+    ]
+  }
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value += 1;
+    this.setState({ counters })
+  }
+
+  handleDelete = (counterId) => {
+    console.log(counterId)
+    const counters = this.state.counters.filter(counter => counter.id !== counterId);
+    this.setState( { counters });
+    console.log(counters)
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState( { counters });
+  }
+  render() { 
+    return (
+      <main className='conatiner'>
+        <NavBar totalCounter={this.state.counters.filter(c => c.value > 0).length }/>
+        <Counters
+          onDelete={this.handleDelete}
+          onIncrement={this.handleIncrement}
+          onReset={this.handleReset}
+          counters={this.state.counters}
+        />
+      </main>
+    );
+  }
 }
-
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
 
